6375f9d991b86431ea9f3725336a29ffcb31c759
ekoeryanto <ekoeryanto@gmail.com>
2020-05-21 06:03:18 +0700

    chore: prepare test netlify

---
+++ scripts/postbuild.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const path = require('path')
+const { spawnSync } = require('child_process')
+
+if (process.env.NETLIFY) {
+  spawnSync(
+    'mv',
+    path.join(__dirname, '..', 'audio'),
+    path.join(__dirname, '..', 'dist/')
+  )
+}
